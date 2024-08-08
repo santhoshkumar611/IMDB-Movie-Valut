9c88a47531c5c9a7304978a0a74eace51342b9ea
santhosh <santhoshsanti329@gmail.com>
2024-08-08 20:22:33 +0530

    new elements

--- movie_valut/src/Components/NavigationBar.jsx
+++ movie_valut/src/Components/NavigationBar.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import logo from '../assets/movie_logo.png'
+import logo from '../assets/Movie_logo2.png'
 import { Link } from 'react-router-dom'
 const NavigationBar = () => {
   return (
     <div className='flex border space-x-8 items-center pl-3 py-4'>
         <img  className='w-[50px]'src={logo} alt="" />
-        <Link to="/" className='text-blue-500 text-3xl font-bold'>Movies</Link>
-        <Link to='/watchlist' className='text-blue-500 text-3xl font-bold'>Watch list</Link>
+        <Link to="/" className='text-blue-500 text-2xl font-bold '>Movies</Link>
+        <Link to='/watchlist' className='text-blue-500 text-2xl font-bold '>Watch list</Link>
     </div>
   )
 }
